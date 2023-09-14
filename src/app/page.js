56028c81c4dbaf98ccca1be2d8cd5cddc54f7baa
tvinchent <tvinchent@gmail.com>
2023-09-14 23:06:20 +0200

    premier commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Bienvenue sur&nbsp;
+          <code className={styles.code}>ma première app !</code>
         </p>
         <div>
           <a
